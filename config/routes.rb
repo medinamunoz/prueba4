11aa35ea536670999fe9d589bc56d5108a7f2ab4
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-03-14 11:49:29 -0300

    running devise and creating model-controllers users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
   resources :complaints
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'complaints#index'
 end
