28552f0d1acb73c9aa0b90fc02ea55755273ab98
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-03-14 13:25:48 -0300

    playing with the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.delete_all
+	20.times do |i|
+		Company.create(name: "Company #{i+1}")
+	end
+User.delete_all
+	30.times do |j|
+		User.create(name: "name #{j+1}", sure_name:"sure_name #{j+1}", email:"name #{j+1}@gmail.com", password:"123456", password_confirmation:"123456")
+	end
+Complaint.delete_all
+	250.times do |k|
+		Complaint.create(name: "Company #{k+1}", theme:"complaint #{k+1}", company_id:"2", user_id:"1")
+	end
